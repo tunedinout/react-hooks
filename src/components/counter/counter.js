5271809563b2bbeefaa8286094ff8b9a04d3035f
tunedinout <narottam2911@gmail.com>
2020-03-02 10:46:19 +0530

    last commit

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -1,24 +1,31 @@
 import React, { useState, useEffect } from 'react'
 
+const changeTitleEffect = (count) => {
+    document.title = `you have clicked ${count} times`
+}
 function Counter() {
-    //use state except intial value of the state variable
+    //use state accepts intial value of the state variable
     //count is started with 0
 
-    //use state hook returns 
+    //useState hook returns an array containing
     //  1. the state variable 
     // 2. call back to change the variable
+
+    //https://reactjs.org/docs/hooks-reference.html#usestate
     const [count, setCount] = useState(0);
     //effects run after the rendering is done
 
+    // 1. 
     useEffect(() => {
         //update the document title
-        document.title = `you have clicked ${count} times`;
+        changeTitleEffect(count);
     }, [count])
     /*<--- tell react only run effect if count changes*/
 
-    //count can props.[some property] 
+    //count can be props.[some property] 
     //tell react to only change a variable based any other values received as props
 
+    // 2. 
     /* useEffect(() => {
         //some logic here
         document.title = `you have clicked ${count} times`;
@@ -27,7 +34,23 @@ function Counter() {
     //This effect will run only once the component is mounted
     //not on every re-render
 
+    //adding the variable will solve the bug 
+    //but if we do not want our component to render so frequently
 
+    //3.
+    // useEffect(() => {
+    //     //update the document tiltle
+    //     document.title = `you have clicked ${count} times`
+
+    //     const id = setInterval(() => {
+    //         // if count has changed only then call setCount and a render of that component??
+
+    //         setCount(count+1);/*if (count) is the argument this effect as [1]*/
+    //     }, 1000);
+
+    //     return () => clearInterval(id);
+
+    // })
 
 
 
